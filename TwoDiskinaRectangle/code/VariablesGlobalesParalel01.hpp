0cca8afce68a4d57eb2602d30364ba730b0667ff
karel <kzapfe@gmail.com>
2017-05-19 17:19:25 -0500

    w=1.5 h=1.0

--- TwoDiskinaRectangle/code/VariablesGlobalesParalel01.hpp
+++ TwoDiskinaRectangle/code/VariablesGlobalesParalel01.hpp
@@ -16,7 +16,7 @@ const double tolerancia=2.0e-16;
 const double tiempomax=66666.0;
 const int colisionesmax=4000; //asi es suficiente
 //const int colisionesmax=16000; //rayando en demasidoconst double widthmedia=0.5, heightmedia=0.5;
-const double widthmedia=0.5, heightmedia=0.5;
+const double widthmedia=0.75, heightmedia=0.5;
 const double radiomax=(widthmedia+heightmedia)-sqrt(2*widthmedia*heightmedia);
 const double radiomaforhop=0.5;
 const double Energia=1.00;
@@ -26,3 +26,4 @@ const double Energia=1.00;
 
 
 
+a
